b8aa66b2e8365cd1c180d738bc71ab9682fe0399
Cleankz <vm3416294@gmail.com>
2023-07-17 22:11:18 +0700

    0

--- autopark/management/commands/gt.py
+++ autopark/management/commands/gt.py
@@ -2,7 +2,7 @@ from django.contrib.gis.geos import MultiPoint, Point
 from faker import Faker
 from django.core.management.base import BaseCommand, CommandError
 from autopark.models import Vehicle, Enterprise, Brand,Driver,Enterprise, Routes
-
+import openrouteservice
 class Command(BaseCommand):
     help = 'add treck'
 
@@ -21,7 +21,15 @@ class Command(BaseCommand):
         for i in range(10):
             point = Point(x=float(faker.longitude()), y=float(faker.latitude()))
             route.route.append(point)
-        print(point)
+        route.save()
         print(route)
+     coords = ((8.34234, 48.23424), (8.34423, 48.26424))
+
+    client = openrouteservice.Client(key='5b3ce3597851110001cf6248170095f78f3641c594c261ec409')
+    routes = client.directions(coords)
+
+    print(routes)
+
+
 
 
