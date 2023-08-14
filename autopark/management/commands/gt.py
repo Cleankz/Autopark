a28071d28e37d30e60c194b75f682163a519752c
Cleankz <vm3416294@gmail.com>
2023-08-14 21:28:02 +0700

    0

--- autopark/management/commands/gt.py
+++ autopark/management/commands/gt.py
@@ -1,11 +1,15 @@
 from pprint import pprint
 
+import geopy
+import numpy as np
+from geopy.distance import geodesic, distance
 from django.contrib.gis.geos import MultiPoint, Point
 from faker import Faker
 from django.core.management.base import BaseCommand, CommandError
 from autopark.models import Vehicle, Enterprise, Brand, Driver, Enterprise, Routes
 import openrouteservice
 import json
+import math
 
 
 class Command(BaseCommand):
@@ -70,18 +74,58 @@ class Command(BaseCommand):
 def next_point(route: Routes):
     x_start, y_start = [float(x) for x in route.points.first().point.split(";")]
     x_finish, y_finish = [float(x) for x in route.finish.split(";")]
+
     data = {
         # "maximum_speed": route.max_speed,
         "preference": "fastest",
         "coordinates": ((x_start, y_start), (x_finish, y_finish)),
     }
 
-    distance = route.max_speed * 10
+    distance_1 = route.max_speed * 360 * 0.1
 
     client = openrouteservice.Client(
         key="5b3ce3597851110001cf6248017011c8bff04d5096aae5b5c8f03c15"
     )
     routes = client.directions(**data)
-    routes.points.create(point=point)
+    bbox = routes["routes"][0]["bbox"]
+    print(type(bbox[0]))
+    # distance = geopy.distance.geodesic(bbox[0:2], bbox[2:4]).km
+    point = geodesic(bbox[0:2], bbox[2:4]).destination(
+        point=geopy.Point(bbox[0:2]), distance=distance, bearing=calc_bearing(*bbox)
+    )
+    # x = np.linspace(bbox[0],bbox[1],10)
+    # y = np.linspace(bbox[2],bbox[4],10)
+    # point = distance.interpolate(bbox[0:2], bbox[2:4])
+    print(distance_1)
+
+    print("POINT", point)
+
+    # print(routes["routes"][0]["bbox"])
+    # y =
+    # point = f"{x};{y}"
+    # routes.points.create(point=point)
 
     pprint(routes)
+
+
+def calc_bearing(lat1, long1, lat2, long2):
+    # Convert latitude and longitude to radians
+    lat1 = math.radians(lat1)
+    long1 = math.radians(long1)
+    lat2 = math.radians(lat2)
+    long2 = math.radians(long2)
+
+    # Calculate the bearing
+    bearing = math.atan2(
+        math.sin(long2 - long1) * math.cos(lat2),
+        math.cos(lat1) * math.sin(lat2)
+        - math.sin(lat1) * math.cos(lat2) * math.cos(long2 - long1),
+    )
+
+    # Convert the bearing to degrees
+    bearing = math.degrees(bearing)
+
+    # Make sure the bearing is positive
+    bearing = (bearing + 360) % 360
+
+    return bearing
